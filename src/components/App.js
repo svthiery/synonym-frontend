a7594991b0395efb36f4846d0615101fd72cd1cc
Sarah Thiery <sarahvthiery@gmail.com>
2021-02-23 16:37:06 -0500

    Add routers

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import '../App.css';
 import Header from "./Header";
 import HomeContainer from "./HomeContainer";
@@ -8,13 +9,28 @@ import UserScores from "./UserScores";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <HomeContainer />
-      <Login />
-      <GameContainer />
-      <Leaderboard />
-      <UserScores />
+    <div className="app">
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/">
+            <HomeContainer />
+            <Leaderboard />
+          </Route>
+          <Route path="/play">
+            <GameContainer />
+          </Route>
+          <Route path="/scores">
+            <Leaderboard />
+            <UserScores />
+          </Route>
+        </Switch>
+      </Router>
+
+      
     </div>
   );
 }
